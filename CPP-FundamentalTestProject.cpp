2cc7d1a47f30cfa051fef693fb54cc0363637907
Myoungmin <miung222@gmail.com>
2022-08-25 11:01:47 +0900

    if문 안에서 논리 연산자를 적용할 때 함수를 모두 실행하는지 확인하는 실습
    // 결론 : 처음 bool로 if문 안의 조건이 결정되면 뒤에 함수는 실행하지 않고 넘어간다.

--- CPP-FundamentalTestProject.cpp
+++ CPP-FundamentalTestProject.cpp
@@ -2,11 +2,18 @@
 #include <string>
 
 void MakeStringAndShow();
+void IF_LogicalOperator();
 
 int main()
 {
-	MakeStringAndShow();
+	//MakeStringAndShow();
+	IF_LogicalOperator();
 }
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+/*  std::to_string 메서드로 int를 string으로 변환, 연산자로 string 합치기, c_str() 메서드로 string에서 const char* 반환 */
+
 
 void MakeStringAndShow()
 {
@@ -33,3 +40,50 @@ void MakeStringAndShow()
 
 	std::cout << stringToChar << std::endl;
 }
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+/*  if문 안에서 논리 연산자를 적용할 때 함수를 모두 실행하는지 확인하는 실습 */
+
+// 결론 : 처음 bool로 if문 안의 조건이 결정되면 뒤에 함수는 실행하지 않고 넘어간다.
+
+bool BoolTrue()
+{
+	std::cout << "BoolTrue" << std::endl;
+	return true;
+}
+
+bool BoolFalse()
+{
+	std::cout << "BoolFalse" << std::endl;
+	return false;
+}
+
+void IF_LogicalOperator()
+{
+	std::cout << "////////// if문 안에 OR 연산자 실습 ///////////" << std::endl;
+
+	// 처음 BoolTrue() 함수만 실행되고 뒤에 함수들은 실행하지 않고 넘어간다.
+	if (BoolTrue() || BoolFalse() || BoolTrue())
+	{
+		std::cout << "****** if문 안 ******" << std::endl;
+	}
+
+	std::cout << "////////// if문 안에 OR 연산자 실습 종료 ///////////" << std::endl;
+
+	std::cout << "////////// if문 안에 AND 연산자 실습 ///////////" << std::endl;
+
+	// 처음 BoolFalse() 함수만 실행되고 뒤에 함수들은 실행하지 않고 넘어간다.
+	if (BoolFalse() && BoolTrue() && BoolFalse())
+	{
+		std::cout << "****** if문 안 ******" << std::endl;
+	}
+
+	std::cout << "////////// if문 안에 AND 연산자 실습 종료 ///////////" << std::endl;
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////

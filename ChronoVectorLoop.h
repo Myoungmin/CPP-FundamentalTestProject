0fdb8614a7e0cdf1b8964063d9f9e27315ab20b0
Myoungmin <miung222@gmail.com>
2022-10-31 21:58:52 +0900

    high_resolution_clock으로 vector 인덱스 기반으로 순회하는 시간 측정

--- ChronoVectorLoop.h
+++ ChronoVectorLoop.h
@@ -7,8 +7,25 @@
 void ChronoVectorLoop()
 {
 	std::size_t n;
-	std::cout << "vector element 개수 입력 :" ;
+	std::cout << "vector element 개수 입력 :";
 	std::cin >> n;
 
 	std::vector<int> nums(n, 1);
+
+
+	auto start = std::chrono::high_resolution_clock::now();
+
+	for (int i = 0; i < 1000; i++)
+	{
+		for (std::size_t idx = 0; idx < nums.size(); idx++)
+		{
+			nums[idx] *= 2;
+		}
+	}
+
+	auto end = std::chrono::high_resolution_clock::now();
+
+	std::chrono::duration<double> diff = end - start;
+
+	std::cout << "Index Loop : " << diff.count() << "s" << std::endl;
 }

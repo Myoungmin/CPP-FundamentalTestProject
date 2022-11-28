11d7b7675485ce6c82720e8a743b2b99d195aae5
Myoungmin <miung222@gmail.com>
2022-11-28 22:02:31 +0900

    omp parallel 사용 테스트, OpenMP를 사용하려면 프로퍼티 설정 필요하다.

--- CPP-FundamentalTestProject.cpp
+++ CPP-FundamentalTestProject.cpp
@@ -1,5 +1,7 @@
 ﻿#include <iostream>
 #include <string>
+#include "omp.h"
+
 #include "ChronoVectorLoop.h"
 #include "OptimallySetMemberString.h"
 #include "ConstTest.h"
@@ -9,6 +11,7 @@ void MakeStringAndShow();
 void IF_LogicalOperator();
 void TestConstTest();
 void SystemMethodTest();
+void OMPParrallelTest();
 
 int main()
 {
@@ -18,7 +21,8 @@ int main()
 	//OptimallySet();
 	//TestConstTest();
 	//DelegatingConstructor();
-	SystemMethodTest();
+	//SystemMethodTest();
+	OMPParrallelTest();
 }
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -32,14 +36,14 @@ void MakeStringAndShow()
 	std::string stringNumber = std::to_string(number);
 
 	// 시행 착오 /////////////////////////////////////////////////////////////////////////////////
-	
+
 	//const char* stringToChar = ("DeviceName" + stringNumber + "appendedName").c_str();
-	 
+
 	// 처음에 위와 같이 출력하려고 했으나 제대로된 문자열이 출력되지 않았다.
 	// c_str()은 원본 객체가 담고있는 문자열에 대해 const char* 타입을 리턴한다.
 	// 이 값은 원본 객체인 'string이 메모리를 재할당'받거나 'string 객체가 사라지는 순간' 무효화된다.
 	// 그래서 메모리에 할당하지 않고 임시로 만든 문자열은 제대로 출력할 수가 없다.
-	 
+
 	//////////////////////////////////////////////////////////////////////////////////////////////
 
 	std::string device = "DeviceName";
@@ -156,6 +160,30 @@ void SystemMethodTest()
 
 	// cmd 창 유지하기 위한 명령어
 	system("cmd /k ipconfig");
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+void fn()
+{
+	// thread의 넘버를 출력한다.
+	std::cout << omp_get_thread_num() << std::endl;
+}
+
+
+void OMPParrallelTest()
+{
+	// 아래는 병렬의 수를 직접 지정하는 것
+	//omp_set_num_threads(2);
+	// 아래만 존재하면 사용자의 CPU 코어 개수를 파악해 코어 수만큼 병렬 실행한다.
+#pragma omp parallel
+	{
+		fn();
+	}
 }
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////

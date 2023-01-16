b9488c62a50c58b16d1728416e3acf3e2dfb3fc3
Myoungmin <miung222@gmail.com>
2023-01-16 22:36:15 +0900

    static을 붙여야 반복마다 값이 유지된다.

--- LoopTest.h
+++ LoopTest.h
@@ -13,7 +13,8 @@ void LoopTest()
 		// 메모리에 저장되 있던 지역변수가 메모리상에서 사라지게 된다. 
 		// 메모리상에서 사라지게 되므로 매 index마다 int nInVariable 는 소멸하고, 
 		// 새로운 int nInVariable가 메모리상에 저장 된다.
-		int nInVariable = 0;
+		//int nInVariable = 0;
+		static int nInVariable = 0;
 		nOutVariable++;
 		nInVariable++;
 		std::cout << "nOutVariable : " << nOutVariable << std::endl;
